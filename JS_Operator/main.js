f3b9b30ba8735448952ac3405b26b5e06b9bff4d
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-02-19 23:19:45 +0900

    feat : 산술

---
+++ JS_Operator/main.js
@@ -0,0 +1,10 @@
+console.log("----------------------");
+console.log("산술, 할당, 증감 연산자");
+console.log("----------------------");
+
+// 산술
+const sum = 20 + 10; // 더하기
+const minus = 20 - 10; // 빼기
+const multiply = 20 * 10; // 곱하기
+const divide = 20 / 5; // 나누기
+const remain = 20 / 6; // 나누기 한것의 나머지
