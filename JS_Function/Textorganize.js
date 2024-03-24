7608547b6fa40c49d4a3f29b9c6706a2f6455fd0
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-03-24 22:01:59 +0900

    feat : this 시작 여기서 getFullName 디버그로 인한 테스트

--- JS_Function/Textorganize.js
+++ JS_Function/Textorganize.js
@@ -1,16 +1,10 @@
-const hello = () => {
-  console.log("Hello");
+const user = {
+  firstName: "Kyojin",
+  lastName: "Hwang",
+  age: 28,
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
 };
 
-// 스케줄
-const timeout = setTimeout(hello, 2000);
-// ex1) 곧바로 종료
-// clearTimeout 으로 인하여 timeout이 실행되지 않는다.
-clearTimeout(timeout);
-
-// ex2) h1 클릭으로 인해 종료
-const h1El = document.querySelector("h1");
-h1El.addEventListener("click", () => {
-  console.log("Clear!");
-  clearTimeout(timeout);
-});
+console.log(user.getFullName());
