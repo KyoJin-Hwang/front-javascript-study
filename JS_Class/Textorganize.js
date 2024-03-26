1d447f2db6c2a54c5317e3d5fcccbfa7bf025b1f
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-03-26 19:35:42 +0900

    feat : 클래스 문법 만들기

--- JS_Class/Textorganize.js
+++ JS_Class/Textorganize.js
@@ -1,8 +1,18 @@
-function User(first, last){
-  this.firstName = first
-  this.lastName = last
+class User {
+  constructor(first, last){
+    this.firstName = first
+    this.lastName = last
+  }
+  getFullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
 }
 
 const kyo = new User('Kyo', 'Jin')
+const owen = new User('Owen', 'Cold')
 
-console.log(kyo)
+console.log(kyo) // User {firstName: 'Kyo', lastName: 'Jin'}
+console.log(owen) // User {firstName: 'Owen', lastName: 'Cold'}
+
+console.log(kyo.getFullName()) // Kyo Jin
+console.log(owen.getFullName()) // Owen Cold
