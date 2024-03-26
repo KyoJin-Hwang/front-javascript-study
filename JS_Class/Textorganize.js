e4bb025f0015f2c7cfe7cdae85ebd9b70dd22bce
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-03-26 19:36:36 +0900

    feat : 콘스트럭쳐

--- JS_Class/Textorganize.js
+++ JS_Class/Textorganize.js
@@ -1,8 +1,10 @@
 class User {
+  // constructor 는 이전의 function User 역할을 한다
   constructor(first, last){
     this.firstName = first
     this.lastName = last
   }
+  // 쉼표를 쓰지않고 함수를 바로 작성한다.
   getFullName(){
     return `${this.firstName} ${this.lastName}`
   }
