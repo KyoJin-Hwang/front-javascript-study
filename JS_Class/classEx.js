168d41b5a4d53bf92109eea73cae7cbdc6e3e082
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-03-29 23:06:16 +0900

    docs : 인스턴스

--- JS_Class/classEx.js
+++ JS_Class/classEx.js
@@ -16,6 +16,15 @@ const a = new A();
 const b = new B();
 const c = new C();
 
-console.log(b instanceof A); // true
-console.log(b instanceof B); // true
-console.log(b instanceof C); // false
+console.log(c instanceof A); // true
+console.log(c instanceof B); // true
+console.log(c instanceof C); // true
+
+console.log(c.constructor === A); // false
+console.log(c.constructor === B); // false
+console.log(c.constructor === C); // true
+
+// fruits는 Array라는 클래스의 인스턴스 이다.
+const fruits = ["Apple", "Banana"];
+
+console.log(fruits.constructor === Array); //true
