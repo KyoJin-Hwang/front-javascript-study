a136d2f23e5995dc9dbf768bb0adbb87ad54e7f6
Owen <owen1127@naver.com>
2024-06-24 13:43:20 +0900

    feat : find

--- JS_StandardObject_Array/Textorganize.js
+++ JS_StandardObject_Array/Textorganize.js
@@ -1,9 +1,3 @@
-const arr = ["A", "B", "C"];
-
-arr.forEach((item) => console.log(item));
-// A -> B -> C // 한번씩 출력
-
-for (let i = 0; i < arr.length; i += 1) {
-  console.log(arr[i]);
-  // A -> B -> C // 위와 동일
-}
+const arr = [5, 8, 130, 12, 44];
+const foundItem = arr.find((item) => item > 10);
+console.log(foundItem); //130
