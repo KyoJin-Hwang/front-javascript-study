b41049df425790ffa271a0a46b1d5a801f19dbfe
Owen <owen1127@naver.com>
2024-06-24 14:13:56 +0900

    test : test

--- JS_StandardObject_Array/Textorganize.js
+++ JS_StandardObject_Array/Textorganize.js
@@ -1,13 +1,3 @@
-const arr = [5, 8, 130, 12, 44];
-const foundItem = arr.findIndex((item) => item > 10);
+const arr = ["A", "B", "C"];
 
-console.log(foundItem); // 130
-
-const users = [
-  { name: "kyo", age: 20 },
-  { name: "owen", age: 25 },
-  { name: "hwang", age: 23 },
-];
-
-const foundUser = users.findIndex((user) => user.age > 22);
-console.log(foundUser); // { name: 'owen', age: 25 }
+arr.forEach((item) => console.log(item));
