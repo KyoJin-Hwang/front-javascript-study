8a43662487273ad02582755070bf0bcaf1704948
Owen <owen1127@naver.com>
2024-06-29 18:43:44 +0900

    feat : slice

--- JS_StandardObject_Array/Textorganize.js
+++ JS_StandardObject_Array/Textorganize.js
@@ -1,5 +1,6 @@
-const arr = ["A", "B", "C"];
+const arr = ["A", "B", "C", "D", "E", "F", "G"];
 
-console.log(arr); // [ 'A', 'B', 'C' ]
-arr.unshift("C", "D", "X");
-console.log(arr); // [ 'C', 'D', 'X', 'A', 'B', 'C' ]
+console.log(arr.slice(0, 3)); // [ 'A', 'B', 'C' ]
+console.log(arr.slice(4, -1)); // [ 'E', 'F' ]
+console.log(arr.slice(5)); // [ 'F', 'G' ]
+console.log(arr); // [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
