002fda74b8b8ca6f5c7fa746c27a08f758a114cb
Owen <owen1127@naver.com>
2024-08-03 02:24:30 +0900

    test :test

--- JS_Asynchronous/Textorganize.js
+++ JS_Asynchronous/Textorganize.js
@@ -1,20 +1,36 @@
-const test = (bool) => {
-  return new Promise((resolve, reject) => {
-    if (bool) {
-      resolve("resolve 성공!");
-    } else {
-      reject("reject 성공!");
-    }
+const getMovies = (movieName) => {
+  return new Promise((resolve) => {
+    fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+      .then((res) => res.json())
+      .then((res) => resolve(res));
   });
 };
 
-test(false)
-  .then((val) => {
-    console.log(val);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    console.log("난 finally 어떠한 경우에도 마지막에 실행되용!");
-  });
+const titles = ["frozen", "avengers", "avatar"];
+
+// forEach는 배열의 순서대로 출력되지않음
+// Promise를 기다리지 않는다 (mdn출처).
+
+// titles.forEach(async (title) => {
+//   const movies = await getMovies(title);
+//   console.log(title, movies);
+// });
+
+// 대안 1 for of
+// const start = async () => {
+//   for (const title of titles) {
+//     const movies = await getMovies(title);
+//     console.log(title, movies);
+//   }
+// };
+// start();
+
+// 대안 2 Promise all
+const start2 = async () => {
+  // const moviePromises = ;
+  const movies = await Promise.all(titles.map(getMovies)).then(
+    (data) => data.Search
+  );
+  console.log(movies);
+};
+start2();
